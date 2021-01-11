4fa7e7491d479e0327ed31f7930e5afb888f9ce5
Ivan Dimitrov <biobeehive@abv.bg>
2021-01-11 20:22:49 +0200

    Initial Commit

---
+++ WiFiColorDossi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WiFiColorDossi
+//
+//  Created by Ivan Dimitrov on 11.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WiFiColorDossi/WiFiColorDossiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiColorDossiApp.swift
+//  WiFiColorDossi
+//
+//  Created by Ivan Dimitrov on 11.01.21.
+//
+
+import SwiftUI
+
+@main
+struct WiFiColorDossiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
